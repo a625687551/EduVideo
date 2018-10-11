e7fb9ec752a57d9e3ebaa6176f8ce8d6906176e8
jianfeng.wang <jianfeng.wang@maixunbytes.com>
2018-10-11 21:00:06 +0800

    first upload

---
+++ EduVideo/spiders/jike_web_all.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from scrapy import Spider
+from scrapy import Request
+
+home_url = "https://www.jikexueyuan.com/course/"
+
+
+class JikeSpider(Spider):
+    name = "jike_all"
+
+    def start_requests(self):
+        self.logger.info("start parse")
+        yield Request(url=home_url, callback=self.parse_list)
+
+    def parse_list(self, response):
+        pass
+
+    def parse_detail(self, response):
+        pass
+
+    def parse_user(self, response):
+        self.logger.info("parse a person")
